d59825168acca22fad6f0f224c173ab8ad1f4c65
akshat-315 <akshat315.sh@gmail.com>
2024-01-11 16:47:40 +0530

    Done with checking the uniqueness of an email while creating a new user.

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRestController.java
@@ -2,6 +2,7 @@ package com.shopme.admin.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,7 @@ public class UserRestController {
     @Autowired
     UserService userService;
 
+    @PostMapping("/users/check_email")
     public String checkDuplicateEmail(@Param("email") String email){
         return userService.isEmailUnique(email) ? "OK" : "Duplicate";
     }
